705a78a717435e3596e99d94f81ee64e622f3208
vaibhav1510 <vaibhav@chargebee.com>
2020-12-09 00:00:02 +0530

    change

--- src/main/java/com/techlearn/app/AppApplication.java
+++ src/main/java/com/techlearn/app/AppApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 
 }
